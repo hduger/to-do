b75f0e18403fa4a82693e881ce66905c2e9aeca5
Hunter Duger <hduger1@gmail.com>
2022-08-13 23:59:12 -0400

    Got Projects working properly with local storage

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
-import addProject from  './project'
+import addProject, { cancel, render, createList, highlight, deleteProject } from  './project'
 
-const projBtn = document.querySelector('.add-project');
+const deleteList = document.querySelector('[data-delete-btn]');
+
+const listContainer = document.querySelector('[data-project-list]');
+
+listContainer.addEventListener('click', e => {
+    highlight(e);
+})
+
+deleteList.addEventListener('click', e =>{
+    deleteProject(e);
+})
+
+
+const projBtn = document.querySelector('.add');
 projBtn.addEventListener('click', addProject)
+
+
+const cancelBtn = document.querySelector('.cancel')
+cancelBtn.addEventListener('click', cancel)
 console.log("hunter is the best!!!")
+
+render()
 
--- src/project.js
+++ src/project.js
@@ -1,11 +1,78 @@
+
+const LOCAL_STORAGE_LIST_KEY = 'task.lists';
+const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId';
+let projectList = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];
+let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY);
+const listContainer = document.querySelector('[data-project-list]');
+const deleteList = document.querySelector('[data-delete-btn]');
+
+
+export function highlight(e){
+    if(e.target.tagName.toLowerCase()=== 'li'){
+        selectedListId= e.target.dataset.listId;
+        renderAndSave();
+    }
+}
+
+
 export default function addProject(){
-    const addProjItem = document.querySelector('.add-project');
+    // const addProjItem = document.querySelector('.add-project');
+    const inputText = document.querySelector('.project-popup-input');
+    const inputVal = inputText.value;
+    if(inputVal == null || inputVal === "") return
 
+    const list = createList(inputVal)
+    inputText.value = null;
+    projectList.push(list);
+    renderAndSave();
 
-    
-    addProjItem.addEventListener('click', ()=>{
-    const form = document.createElement('input');
-    form.innerText='hello world'
-    document.body.appendChild(form);
-})
+    console.log('click')
+
+}
+
+export function createList(name){
+    return {id: Date.now().toString(), name: name, tasks: [] }
+}
+
+export function cancel(){
+    const textToClear = document.querySelector('.project-popup-input');
+    textToClear.value = "";
+    console.log('cancel')
+
+}
+
+export function renderAndSave(){
+    save();
+    render();
+}
+
+export function save(){
+    localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(projectList))
+    localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId)
+}
+
+export function render(){
+    clearList(listContainer)
+    projectList.forEach(list => {
+        const listElement = document.createElement('li');
+        listElement.dataset.listId = list.id;
+        listElement.classList.add('project-name');
+        listElement.innerText = list.name;
+        if (list.id === selectedListId) {
+            listElement.classList.add("active-list");
+        }
+        listContainer.appendChild(listElement);
+    })
+}
+
+export function clearList(element){
+    while (element.firstChild){
+        element.removeChild(element.firstChild)
+    }
+}
+
+export function deleteProject(){
+    projectList = projectList.filter(list => list.id !== selectedListId)
+    selectedListId = null;
+    renderAndSave();
 }
